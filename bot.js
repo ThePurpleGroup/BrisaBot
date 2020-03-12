4732b88fcaec23450f1ec290c010b62faae39837
marceloranngel <marcelo.rcs@icloud.com>
2020-03-11 21:37:18 -0300

    commit

---
+++ bot.js
@@ -0,0 +1,66 @@
+const Twit = require('twit');
+const config = require('./config.js');
+const tweetBot = new Twit(config);
+
+const params = {
+    q: 'Maconha',
+    result_type: 'popular',
+    count: 70,
+    lang: 'pt'
+}
+
+const nearparams = {
+    q: 'Maconha',
+    result_type: 'recent',
+    count: 30,
+    lang: 'pt'
+}
+
+
+tweetBot.get('search/tweets', params,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Retweet successful` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+            tweetBot.post('favorites/create', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Favorite successful` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
+
+tweetBot.get('search/tweets', nearparams,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Retweet Recent successful` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+            tweetBot.post('favorites/create', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Favorite Recent successful` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
